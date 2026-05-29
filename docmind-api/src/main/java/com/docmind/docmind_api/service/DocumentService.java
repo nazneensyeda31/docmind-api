9e4266ac84b6629acccd966fa974720f205e1a13
syedanazneen25 <syedanazneen2501@gmail.com>
2026-05-29 07:52:25 -0700

    add Service layer

---
+++ docmind-api/src/main/java/com/docmind/docmind_api/service/DocumentService.java
@@ -0,0 +1,43 @@
+package com.docmind.docmind_api.service;
+
+import com.docmind.docmind_api.domain.entity.Document;
+import com.docmind.docmind_api.domain.entity.DocumentStatus;
+import com.docmind.docmind_api.domain.entity.User;
+import com.docmind.docmind_api.dto.DocumentResponse;
+import com.docmind.docmind_api.dto.DocumentUploadRequest;
+import com.docmind.docmind_api.repository.DocumentRepository;
+import com.docmind.docmind_api.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+
+public class DocumentService {
+    private final DocumentRepository documentRepository;
+    private final UserRepository userRepository;
+
+    public DocumentResponse uploadDocument(DocumentUploadRequest request) {
+        User user = userRepository.findById(request.getUserId()).orElseThrow(()->
+                new RuntimeException("User Not Found"));
+
+        Document document = Document.builder().fileName(request.getFileName())
+                .fileSize(request.getFileSize())
+                .status(DocumentStatus.UPLOADED)
+                .user(user)
+                .build();
+
+        Document saved = documentRepository.save(document);
+
+        return DocumentResponse.builder()
+                .id(saved.getId())
+                .fileName(saved.getFileName())
+                .fileSize(saved.getFileSize())
+                .status(saved.getStatus())
+                .uploadedAt(saved.getUploadedAt())
+                .userId(saved.getUser().getId())
+                .build();
+    }
+}
